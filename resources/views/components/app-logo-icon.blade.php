9ea014acff6400eddeaa02f4a3f478ed2ecafe73
MFarix12 <m.farissufi06@gmail.com>
2025-07-13 16:56:29 +0800

    yes

--- resources/views/components/app-logo-icon.blade.php
+++
@@ -1,8 +0,0 @@
-<svg width="160" height="48" viewBox="0 0 160 48" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-  <rect width="160" height="48" rx="14" fill="#DC2626"/>
-  <text x="50%" y="54%" text-anchor="middle" dominant-baseline="central" font-family="Montserrat, Arial, sans-serif" font-size="28" font-weight="900" fill="#fff" letter-spacing="-1.5">
-    ScoreUp
-  </text>
-  <rect x="40" y="36" width="80" height="6" rx="3" fill="#F97316"/>
-  <polygon points="120,36 130,36 125,26" fill="#F97316"/>
-</svg>
--- resources/views/components/flux-card.blade.php
+++
@@ -1,3 +0,0 @@
-<div {{ $attributes->merge(['class' => 'rounded-xl bg-white dark:bg-neutral-900 border border-neutral-200 dark:border-neutral-800 shadow-md']) }}>
-    {{ $slot }}
-</div>
